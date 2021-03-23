cbc99d9f5b8b175c19ba21f253ace98a9a7b755f
Sistecma <admin@sistecma.com>
2021-03-23 09:51:47 -0500

    Agregando tests

--- app/go/hola/business/workflow_test.go
+++ app/go/hola/business/workflow_test.go
@@ -3,33 +3,28 @@ package business
 import (
 	"testing"
 
-	"github.com/stretchr/testify/suite"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 	"go.temporal.io/sdk/testsuite"
 )
 
-type UnitTestSuite struct {
-	suite.Suite
-	testsuite.WorkflowTestSuite
-}
+func Test_Workflow(t *testing.T) {
+	testSuite := &testsuite.WorkflowTestSuite{}
+	env := testSuite.NewTestWorkflowEnvironment()
 
-func TestUnitTestSuite(t *testing.T) {
-	suite.Run(t, new(UnitTestSuite))
-}
-
-func (s *UnitTestSuite) Test_Workflow() {
-	env := s.NewTestWorkflowEnvironment()
-
-	// Realizar la prueba para la actividad individualmente
-	// En el caso de este ejemplo la actividad es la función Activity
-	env.OnActivity(Activity, "Sistecma").Return("Hola Sistecma", nil)
+	// Mock para la actividad Activity del archivo hello.go
+	env.OnActivity(Activity, mock.Anything, "Sistecma").Return("Hola Sistecma", nil)
 
 	// Realizar la prueba del workflow
-	// En el caso de este ejemplo el workflow es la función Workflow
-	env.ExecuteWorkflow(Workflow)
+	// En el caso de este ejemplo el workflow es la función Workflow del archivo hello.go
+	env.ExecuteWorkflow(Workflow, "Sistecma")
 
 	// Validamos que el workflow este completo y que no hayan errores
-	s.True(env.IsWorkflowCompleted())
-	s.NoError(env.GetWorkflowError())
+	require.True(t, env.IsWorkflowCompleted())
+	require.NoError(t, env.GetWorkflowError())
 
-	env.AssertExpectations(s.T())
+	// Obtenemos y Evaluamos el resultado esperado del workflow
+	var result string
+	require.NoError(t, env.GetWorkflowResult(&result))
+	require.Equal(t, "Hola Sistecma", result)
 }
--- app/go/hola/go.mod
+++ app/go/hola/go.mod
@@ -3,6 +3,7 @@ module hola
 go 1.15
 
 require (
+	github.com/stretchr/testify v1.6.1
 	github.com/temporalio/samples-go v1.3.0 // indirect
 	go.temporal.io/sdk v1.4.1
 )
